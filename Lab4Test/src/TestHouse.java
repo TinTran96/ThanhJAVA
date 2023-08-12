cb7069867636679f12b6030168466118f2c4682b
KThanhTran <Thanhtran14012006@gmail.com>
2023-08-12 11:47:13 +0700

    Submit

--- Lab4Test/src/TestHouse.java
+++ Lab4Test/src/TestHouse.java
@@ -4,6 +4,14 @@ public class TestHouse {
 	public static void prepareData(House[] houseList){
 		houseList[0] = new House("A01", 2, 100.0, 100.0, false);
 		houseList[1] = new House("A02", 4, 200.0, 200.0, true);
+		for(int i = 0; i < 2; i++){
+			System.out.printf("====o%do======", i+1);
+			System.out.println("\nHouse code: " + houseList[i].getHouseCode());
+			System.out.println("Bedrooms: "+ houseList[i].getNumOfBedRooms());
+			System.out.println("Area: " + houseList[i].getArea());
+			System.out.println("Cost per square meter: " + houseList[i].getCostPerSquareMeter());
+			
+		}
 	}
 	
 	public static void sum(House[] houseList, double[] subTotal, double[] sellingPrice){
